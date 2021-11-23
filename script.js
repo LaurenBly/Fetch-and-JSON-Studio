a88f46967f7f4603e229611ef69f1efd7e3d2fe5
LaurenBly <lauren.d.bly@gmail.com>
2021-11-22 19:39:49 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// TODO: add code here
+window.addEventListener("load", event => {
+    let jsonDat = [];
+    let content = document.getElementById("content");
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(event => {
+         event.json().then(astronautList => {
+
+            for(let i =0; i <astronautList.length; i++){
+                
+            
+            content.innerHTML = `<div class="astronaut">
+            <div class ="bio">
+            <h3>${astronautList[i].firstName} ${astronautList[i].lastName}</h3>
+                <ul>
+                    <li>${astronautList[i].hoursInSpace}</li>
+                    <li `;
+                    if({astronautList[i].active} === true) {
+                        content.innerHTML += `class="active"`;
+                    } else {
+                        content.innerHTML +=``;
+                    }    
+
+                     content.innerHTML += `>Active: ${astronautList[i].active}</li>
+                    <li>Skills: ${astronautList[i].skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${astronautList[i].picture}">
+            </div>`;
+            }
+          
+        });  
+    
+    });
+});
